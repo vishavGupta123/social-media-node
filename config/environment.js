1ceb995d9118a0b91834030e1cf55d38e6ba4f74
Vishav Gupta <vishavgupta@bubusiness.in>
2021-03-06 21:56:08 +0530

    Production development done

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,16 @@
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+const path = require('path');
+
+
+const logDirectory = path.join(__dirname,"../production_logs");
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs.createStream('access.log',{
+    interval:'1d',
+    path: logDirectory
+})
+
 const development = {
     name: 'development',
     asset_path: './assets',
@@ -16,10 +29,34 @@ const development = {
     google_client_id:"1048787061973-340ck3cu278s7mflki0l97fms2cifq67.apps.googleusercontent.com",
     google_client_secret:"C4Q2bwiV2tJM24dgnivwUyI7",
     google_call_back_url:'http://localhost:8000/users/auth/google/callback',
+    morgan:{
+        mode:'dev',
+        options:{stream: accessLogStream}
+    }
 }
 
 const production = {
-    name: 'production'
+    name: 'production',
+    asset_path: process.env.CODEIAL_ASSET_PATH,
+    session_cookie_key:process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db:process.env.CODEIAL_DB,
+    smtp:{
+        service:'gmail',
+        host:'smtp.gmail.com',
+        port:587,
+        secure:false,
+        auth:{
+            user:process.env.CODEIAL_GOOGLE_USERNAME,
+            pass:process.env.CODEIAL_GOOGLE_PASSWORD
+        }
+    },
+    google_client_id:process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_secret:process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_call_back_url:process.env.CODEIAL_GOOGLE_CALLBACK_URL,
+    morgan:{
+        mode:'combined',
+        options: {stream: accessLogStream}
+    }
 }
 
-module.exports = development;
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const env = require('./config/environment');
+const logger = require('morgan');
 const app = express();
+require('./config/view-helpers')(app);
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
@@ -22,19 +24,24 @@ chatServer.listen(5000);
 console.log('chat server is listening on port 5000');
 const path = require('path');
 
+if(env.name=="development"){
+    app.use(sassMiddleware({
+        src:path.join(__dirname,env.asset_path,'/scss'),
+        dest:path.join(__dirname,env.asset_path,'css'),
+        debug:true,
+        outputStyle:'extended',
+        prefix:'/css'
+    }))
+}
 
-app.use(sassMiddleware({
-    src:path.join(__dirname,env.asset_path,'/scss'),
-    dest:path.join(__dirname,env.asset_path,'css'),
-    debug:true,
-    outputStyle:'extended',
-    prefix:'/css'
-}))
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static(env.asset_path));
 //make the uploads part available to the browser
 app.use('/uploads',express.static(__dirname+'/uploads'));
+
+app.use(logger(env.morgan.mode,env.morgan.options));
+
 app.use(expressLayouts);
 
 
